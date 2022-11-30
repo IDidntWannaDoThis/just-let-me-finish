f28889db9666148f61b8840343063a719ff33175
klae0061 <lae0061@ed.campusvejle.dk>
2022-11-30 12:57:36 +0100

    done

--- Untitled Folder/S0067_rpg2.py
+++ Untitled Folder/S0067_rpg2.py
@@ -29,4 +29,71 @@ When your program is complete, push it to your github repository.
 Then send this Teams message to your teacher: <filename> done
 Thereafter go on with the next file.
 """
+import random
 
+class Character:
+
+    def __init__(self, name = "" , health = 100, attackpower = 10):
+        self.Max_health = health
+        self._current_health = self.Max_health
+        self.attackpower = attackpower
+        self.name = name
+
+    def __repr__(self):
+        return f"name:{self.name}. health:{self._current_health}. max health:{self.Max_health}. attackpower:{self.attackpower}."
+
+    def hit(self, victem):
+        victem.get_hit(self)
+
+    def get_hit(self, attacker):
+        self._current_health -= int(attacker.attackpower * self.rangecalc())
+
+    def get_healed(self, healer):
+        self._current_health += int(healer.healpower * self.rangecalc())
+
+    def get_health(self):
+        return self._current_health
+
+    def rangecalc(self):
+        return float(random.randint(75,125)) / float(100)
+
+class Healer(Character):
+
+    def __init__(self, name ="", health = 75, healpower=15, attack= 1):
+        self.healpower = healpower
+        self.Max_health = health
+        self._current_health = self.Max_health
+        self.name = name
+        self.attackpower = attack
+
+    def __repr__(self):
+        return f"name:{self.name}. health:{self._current_health}. max health:{self.Max_health}. attackpower:{self.attackpower}. healpower:{self.healpower}."
+
+
+    def heal(self, other):
+        other.get_healed(self)
+
+war1 = Character("jack", 100,20)
+war2 = Character("max", 100, 22)
+med = Healer("jess", 75, 15)
+
+def round (war1, war2):
+    war1.hit(war2)
+    if war2.get_health() <= 0:
+        return
+    war2.hit(war1)
+    if war1.get_health() <= 0:
+        return
+
+def match(rounds, fighter1,fighter2):
+    for i in range(rounds):
+        round(fighter1,fighter2)
+        print(war1)
+        print(war2)
+        print()
+        if fighter2.get_health() <= 0:
+            return fighter1
+        elif fighter1.get_health() <= 0:
+            return fighter2
+
+print(match(100,war1,war2),"wins")
